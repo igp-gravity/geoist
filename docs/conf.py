346e21f08e00145ac1308110ad2cea788676344f
rular099 <rular099@gmail.com>
2018-11-24 19:39:21 +0800

    conda is too slow for readthedocs, swith back to pip. let sphinx ignore import error of gdal and cartopy.

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
 ]
-
+autodoc_mock_imports = ["gdal","pygmi","cartopy"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
