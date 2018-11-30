9fc20b4719c6b884cb11672ec47cb06cd9dd8da0
Shi Chen <chenshi@cea-igp.ac.cn>
2018-11-30 10:18:20 +0800

    Update __init__.py

--- geoist/pfm/__init__.py
+++ geoist/pfm/__init__.py
@@ -1,2 +1,8 @@
 from .grdio import regular, spacing
 from .euler import EulerDeconv, EulerDeconvMW, EulerDeconvEW
+from .tide import TideModel
+from .igrf import IGRF
+from .pftrans import reduce_to_pole
+from .normgra import *
+from .giutils import *
+from .giconstants import *
