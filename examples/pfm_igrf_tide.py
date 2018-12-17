b26e553fedd732c6a2c7b5e145448a1f293de741
Shi Chen <chenshi@cea-igp.ac.cn>
2018-12-17 12:04:42 +0800

    fix igrf

--- examples/pfm_igrf_tide.py
+++ examples/pfm_igrf_tide.py
@@ -6,13 +6,17 @@
 
 #third party imports
 # local imports
-import datetime
+from datetime import datetime
 import geoist.pfm as gip
 
 m1=gip.IGRF()
-m1.pnt(45,105,100)
-
+aa=m1.pnt(45,105,100)
+print(aa)
 g1=gip.TideModel()
-gdate = datetime.datetime(2018, 11, 1, 12, 00, 00)    
-gm, gs, g =g1.solve_longman(45,105,100,gdate)
+gdate = datetime(2018, 11, 1, 12, 00, 00)  
+gm, gs, g =g1.solve_longman(45,105,100,gdate)  
+
+value='2018-11-01 12:00:00'
+gdd=datetime.strptime(value,'%Y-%m-%d %H:%M:%S')
+gm, gs, g =g1.solve_longman(45,105,100,gdd)
 print("Earthtide value in this time is:",g)
