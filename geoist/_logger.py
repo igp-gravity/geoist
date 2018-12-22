67517fc0afb9ca29f2c62ac18ba61fbf4934471d
Shi Chen <chenshi@cea-igp.ac.cn>
2018-12-22 11:27:28 +0800

    add log func
    geoist.log.info() ...

---
+++ geoist/_logger.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 15 18:00:46 2018
+
+@author: chens
+"""
+
+import logging
+import pathlib
+
+def setlogger(modname=__name__, level = logging.INFO, logfile = 'geoist.log'):
+    logger = logging.getLogger(modname)
+    logger.setLevel(level=level)
+    filepath = pathlib.Path(__file__).parent
+    handler = logging.FileHandler(pathlib.Path(filepath,logfile))
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+logger = setlogger()
+
+def setlevel(level = 20):
+    logger.setLevel(level=level)
+
+def setname(logname = __name__):
+    logger.name = logname
+
+def debug(str='Debugging'):  #10
+    logger.debug(str)
+      
+def info(str='INFO'):   #20
+    logger.info(str)
+
+def warning(str='Warning'):  #30
+    logger.warning(str)
+    
+def error(str='ERROR'):  #40
+    logger.error(str)
+
+def critical(str='CRITICAL'):  #50
+    logger.critical(str)
