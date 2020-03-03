31f8ecfb1621fa5c9acd5fdbbbf0771f8f4cbbea
Shi Chen <chenshi@cea-igp.ac.cn>
2020-03-03 12:42:42 +0800

    init error

--- geoist/inversion/__init__.py
+++ geoist/inversion/__init__.py
@@ -354,5 +354,4 @@ from .hyper_param import LCurve
 from .geometry import Polygon, Square, Prism, Tesseroid, Sphere
 from .geometry import PolygonalPrism
 from .mesh import SquareMesh, PointGrid, PrismRelief, PrismMesh, TesseroidMesh
-from .pfmodel import *
 
